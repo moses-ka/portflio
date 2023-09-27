55ee090a11cafdf5b1831e81b3cc1b70cfebe867
moses ka <you@example.com>
2023-09-27 20:43:14 +0200

    project sec almost ready

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,7 +13,7 @@ export default function NavBar() {
         <>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
-                    <span>Logo</span>
+                    <span></span>
                     <button
                         onClick={toggle}
                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -33,14 +33,16 @@ export default function Projects() {
          <div className='flex flex-col justify-center items-center md:flex-row gap-4'>
              {projects && projects.map((project) => {
                  return (
-                     <div id='card' className="max-w-sm  max-h-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                         <img className="rounded-t-lg" src={project.photo} alt="" />
-                         <div className="p-5">
-                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
-                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.description}</p>
-                             <a href={project.url} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-pruple-800 focus:ring-4 focus:outline-none focus:ring-pruple-300 dark:bg-pruple-600 dark:hover:bg-pruple-700 dark:focus:ring-pruple-800">
+                     <div id='card' className=" w-[340px] md:w-[400px]  h-[600px] flex flex-col justify-center items-center gap-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                         <img className="rounded-t-lg h-96 mb-4" src={project.photo} alt="" />
+                         <div className="p-4  h-full flex flex-col justify-center items-center">
+                             <h5 className="p-4 h-18 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
+                             <p className="mb-2  h-56 p-2 font-normal text-gray-700 dark:text-gray-400">{project.description}</p>
+                             <a href={project.url} className="inline-flex items-center 
+                             p-2 text-sm font-medium text-center text-white bg-purple-700
+                              rounded-lg hover:bg-pruple-800 focus:ring-4 focus:outline-none focus:ring-pruple-300 dark:bg-pruple-600 dark:hover:bg-pruple-700 dark:focus:ring-pruple-800">
                                  Read more
-                                 <svg className="w-3.5 h-3.5 ml-2" aria-label="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                 <svg className="w-4 h-4 ml-2" aria-label="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                  </svg>
                              </a>
